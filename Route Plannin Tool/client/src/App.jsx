8f3f68f9214a075fc577277cf35b0733a9a744ea
Rupesh Thakur <rupeshthakur80078@gmail.com>
2024-05-05 12:12:22 +0530

    changes

--- Route Plannin Tool/client/src/App.jsx
+++ Route Plannin Tool/client/src/App.jsx
@@ -51,20 +51,20 @@ function App() {
   const handlePlanRoute = async () => {
     const homeAddressCoordinates = await Promise.all(homeAddresses.map(address => geocodeAddress(address)));
     const technicianAddressCoordinates = await Promise.all(technicianAddress.map(address => geocodeAddress(address)));
-  
+
     const combinedAddressesData = {
       "Addresses": homeAddresses.map((address, index) => ({ address, coordinates: homeAddressCoordinates[index] })),
       "Technician_Address": technicianAddress.map((address, index) => ({ address, coordinates: technicianAddressCoordinates[index] }))
     };
-  
+
     setCombinedAddresses(combinedAddressesData);
-  
+
     const techniciandata = {
       location: combinedAddressesData.Technician_Address[0].address,
       longitude: combinedAddressesData.Technician_Address[0].coordinates[0],
       latitude: combinedAddressesData.Technician_Address[0].coordinates[1]
     };
-  
+
     fetch(`${connect}/api/technician`, {
       method: 'POST',
       headers: {
@@ -92,40 +92,43 @@ function App() {
       .catch(error => {
         console.error('Error storing technician data:', error);
       });
-  
+
     setRoutePlanned(true);
   };
-  
+
   return (
     <div className="App">
       {routePlanned && technicianLocation !== null ? (
         <Map technicianLocation={technicianLocation} />
       ) : (
-        <div className="absolute inset-x-0 top-0 flex items-center justify-center">
-          <div className="p-5 bg-transparent rounded-lg shadow-lg">
-            <Inputs
-              placeholder="Enter Work Address"
-              onAdd={(address) => setHomeAddresses([...homeAddresses, address])}
-              allAddresses={homeAddresses}
-              setAllAddresses={setHomeAddresses}
-              keyType="home"
-            />
-            <Inputs
-              placeholder="Enter Technician Address"
-              onAdd={(address) => setTechnicianAddress([...technicianAddress, address])}
-              allAddresses={technicianAddress}
-              setAllAddresses={setTechnicianAddress}
-              keyType="technician"
-            />
-            <form onSubmit={(e) => e.preventDefault()}>
-              <button
-                type="button"
-                className="plan-route-btn bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md mt-2"
-                onClick={handlePlanRoute}
-              >
-                Plan Route
-              </button>
-            </form>
+        <div className="absolute inset-x-0 top-0 flex items-center justify-center mt-5">
+          <div className="relative">
+            <div className="absolute inset-0 rounded-lg bg-opacity-50 backdrop-filter backdrop-blur-lg"></div>
+            <div className="p-5 bg-transparent rounded-lg shadow-lg relative z-10">
+              <Inputs
+                placeholder="Enter Work Address"
+                onAdd={(address) => setHomeAddresses([...homeAddresses, address])}
+                allAddresses={homeAddresses}
+                setAllAddresses={setHomeAddresses}
+                keyType="home"
+              />
+              <Inputs
+                placeholder="Enter Technician Address"
+                onAdd={(address) => setTechnicianAddress([...technicianAddress, address])}
+                allAddresses={technicianAddress}
+                setAllAddresses={setTechnicianAddress}
+                keyType="technician"
+              />
+              <form onSubmit={(e) => e.preventDefault()}>
+                <button
+                  type="button"
+                  className="plan-route-btn bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md mt-2"
+                  onClick={handlePlanRoute}
+                >
+                  Plan Route
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       )}
